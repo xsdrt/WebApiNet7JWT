4b0cff42803795baf82803b407fde4c197ec20c4
xsdrt <m_redinger@hotmail.com>
2023-01-24 11:18:15 -0500

    Added the Nuget pkg BCrypt.Net-Next to the project a state of the art encryption pkg.

---
+++ WebApiNet7JWT/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiNet7JWT.Models;
+
+namespace WebApiNet7JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+    }
+}
---
+++ WebApiNet7JWT/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiNet7JWT.Models
+{
+    public class UserDto
+    {
+        // FYI: In case wondering , not storing the User in a database for this example so no Id required...
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
